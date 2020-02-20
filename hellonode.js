07743fea73137f62893ea2e7e455d4b305906ef0
Ruben Bramasco <Ruben.Bramasco@Student.Csulb.edu>
2020-02-19 21:37:29 -0800

    Delete hellonode.js

--- hellonode.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 5559
-const path = require('path')
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/ex3.html')))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
