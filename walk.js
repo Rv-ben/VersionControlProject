bb9f30d5bfb46c87ad1048e6608dcc16c0806ee0
mlminer1129 <mlminer1129@gmail.com>
2020-02-24 18:02:11 -0800

    First Update
    Not reading directories properly

--- walk.js
+++ walk.js
@@ -1,1 +1,15 @@
-console.log(require('path').parse('C:\Users\MLMin\OneDrive\Desktop\CECS-343'))
+const { resolve } = require('path');
+const { readdir } = require('fs').promises;
+
+async function getFiles(dir) {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  const files = await Promise.all(dirents.map((dirent) => {
+    const res = resolve(dir, dirent.name);
+    return dirent.isDirectory() ? getFiles(res) : res;
+  }));
+  return Array.prototype.concat(...files);
+}
+
+getFiles('C:\Users\MLMin\OneDrive\Desktop\CECS-343')
+  .then(files => console.log(files))
+  .catch(e => console.error(e));
