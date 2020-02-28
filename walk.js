a724ebc3415e110fedda3d08b6cb9d698761de7c
mlminer1129 <mlminer1129@gmail.com>
2020-02-28 14:40:38 -0800

    third push with working walk
    Walk is now working

--- walk.js
+++ walk.js
@@ -4,12 +4,21 @@ const fs = require('fs');
 //joining path of directory rn only reads Documents and path has to be set
 const directoryPath = path.join(__dirname, 'Documents');
 
+fileWalk(directoryPath);
+
+
+function fileWalk(directoryPath){
 fs.readdir(directoryPath, function (err, files) {
     //error checking
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     } 
+    
     files.forEach(function (file) {
+        if(files.isdirectory == true){
+            fileWalk(file)
+        }
         console.log(file); 
     });
 });
+}
