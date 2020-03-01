41614dbcd4a6244596d14a942a9cfc400a4eb0f6
Rv-ben <rubenbramasco@gmail.com>
2020-02-29 22:37:38 -0800

    Added basic server control

---
+++ webPageShell.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var ex = express();
+var BodyParser =  require('body-parser');
+var port = 5224;
+var cmdParser = require('./commandParser');
+
+
+ex.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/shellFrontEnd.html'));
+})
+
+ex.listen(port,function(){
+    console.log(port);
+})
+
+ex.use(BodyParser.json())
+ex.use(BodyParser.urlencoded({ extended: true }))
+
+ex.post('/cmdForm', function(req,res){
+    console.log(req.body.cmd)
+    cmdParser.parse(req.body.cmd)
+    res.end()
+})
+
