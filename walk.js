e652e82c4d874c913cd79fa62d3cbe1a8e32bdc7
Rv-ben <rubenbramasco@gmail.com>
2020-02-29 23:01:39 -0800

    Fixed list appending

--- walk.js
+++ walk.js
@@ -2,30 +2,27 @@
 const path = require('path');
 const fs = require('fs');
 //joining path of directory rn only reads Documents and path has to be set
-const directoryPath = path.join(__dirname);
 var compute = require('./pathName')
 
 module.exports= {
     walk: fileWalk
 }
 
-fileWalk(directoryPath);
-
 
 function fileWalk(directoryPath){
-    listofCal= []
-fs.readdir(directoryPath, function (err, files) {
+    listofCal= [];
+    fs.readdir(directoryPath, function (err, files) {
     //error checking
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    console.log(directoryPath)
-    files.forEach(function (file) {
-        if(fs.statSync(directoryPath+"/"+file).isDirectory()){
-            fileWalk(directoryPath+"/"+file)
+        if (err) {
+           return console.log('Unable to scan directory: ' + err);
         }
-        else listofCal.append(compute.calc(directoryPath,file)); 
+        files.forEach(function (file) {
+            if(fs.statSync(directoryPath+"/"+file).isDirectory()){
+                fileWalk(directoryPath+"/"+file)
+            }
+            else listofCal.push(compute.calc(directoryPath,file)); 
+        });
+        console.log(listofCal.toString())
     });
     return listofCal;
-});
 }
