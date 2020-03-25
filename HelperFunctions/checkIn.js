ec0a5623e35beea12d4be05b0bc58d5dc3b4d31f
mlminer1129 <mlminer1129@gmail.com>
2020-03-24 17:33:28 -0700

    Create checkIn.JS
    First push.

---
+++ HelperFunctions/checkIn.js
@@ -0,0 +1,13 @@
+//accept input from user
+var checkIn;
+//need to maintain version sizes
+var previousVersionSize;
+//calculate size of previous version to check for changes being pushed
+var sum = pathName.calculateStringSum(checkIn);
+var versionCount;
+//check to see if artifact ID has changed and replace if yes
+if (sum != previousVersionSize){
+	versionCount += 1;
+	return previousVersionSize = sum;
+}
+    versionCount += 1;
