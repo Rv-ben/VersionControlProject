3f798306147eb48b847c4c272230c718d1a487f8
ColinRayburn <47834560+ColinRayburn@users.noreply.github.com>
2020-03-29 13:40:38 -0700

    update copy file with a walk copy method to walk a given directory and copy everything to a new file

--- HelperFunctions/copy.js
+++ HelperFunctions/copy.js
@@ -7,3 +7,56 @@ function copyFile(targetFile, targetFolder){
         if (err) throw err;
       });
 }
+/////////////////////////////////////////////////////////////////////////////
+//modify walk to copy files over to the new location
+//once in new file walk again but regularly to rename all files with new artifact id
+
+
+//initializing path and fs
+const path = require('path');
+const fs = require('fs');
+//joining path of directory rn only reads Documents and path has to be set
+var compute = require('./pathName')
+
+module.exports= {
+    walk: fileWalk
+}
+
+//Walks a folder recursively 
+async function fileWalkCopy(directoryPath, target){
+    listofCal= [];
+    
+    //access the folder
+    fs.readdir(directoryPath, async function (err, files) {
+        //error checking
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+
+        //fs.appendFile('Manifest.txt',"Directory: "+directoryPath+"\n" ,function(error){}) 
+        //fs.appendFile('Manifest.txt',"--------------------------------\n",function(error){})
+
+        //For every folder or directory
+        files.forEach(async function (file) {
+            //if is directory 
+            if (fs.statSync(directoryPath + "/" + file).isDirectory()) {
+              copyFile(file,target);
+                //walk
+                //fs.appendFile('Manifest.txt',"Directory :  "+file+"\n",function(error){})
+                //fileWalk(directoryPath + "/" + file);
+
+            }
+            //if not a dot file get calcs
+            else if (!file.startsWith(".")) {
+              copyFile(file,target);
+
+                //fs.appendFile('Manifest.txt',file + "     " + compute.calc(directoryPath, file)+"\n",function(error){})
+            }
+        });
+        //fs.appendFile('Manifest.txt',"--------------------------------\n",function(error){})
+        //fs.appendFile('Manifest.txt',"\n\n",function(error){})
+    });
+}
+//////////////////////////////////////////////////////////////////////////
+// still need to get a search method to find what manifest to use 
+// pass manifest top level path to the new walk method 
