81fb607799013e6c1889e65ff4a3aee686ad503e
ColinRayburn <47834560+ColinRayburn@users.noreply.github.com>
2020-03-31 10:45:34 -0700

    updating check out almost there

--- HelperFunctions/copy.js
+++ HelperFunctions/copy.js
@@ -7,6 +7,15 @@ function copyFile(targetFile, targetFolder){
         if (err) throw err;
       });
 }
+
+
+function copyFolder(folderToCopy, whereTo, folderName){
+    const fs = require('fs-extra');
+    //takes two arguements the folder in the current directory and the new folder to add it to
+    fs.copy(folderToCopy, whereTo+'/'+ folderName, err =>{
+      if(err) return console.error(err);
+    });
+}
 /////////////////////////////////////////////////////////////////////////////
 //modify walk to copy files over to the new location
 //once in new file walk again but regularly to rename all files with new artifact id
@@ -18,12 +27,10 @@ const fs = require('fs');
 //joining path of directory rn only reads Documents and path has to be set
 var compute = require('./pathName')
 
-module.exports= {
-    walk: fileWalk
-}
+
 
 //Walks a folder recursively 
-async function fileWalkCopy(directoryPath, target){
+async function fileWalkCopy(directoryPath, target, repo){
     listofCal= [];
     
     //access the folder
@@ -37,10 +44,24 @@ async function fileWalkCopy(directoryPath, target){
         //fs.appendFile('Manifest.txt',"--------------------------------\n",function(error){})
 
         //For every folder or directory
+        repoFolder = directoryPath;
+        copyFolder(directoryPath, target, repo);
+
+        repoInNew = target + '/' + repo;
+        inFolder = false;
+        currentFolder = repoInNew;
+
         files.forEach(async function (file) {
+            
+
             //if is directory 
             if (fs.statSync(directoryPath + "/" + file).isDirectory()) {
-              copyFile(file,target);
+                currentFolder = repoInNew + "/" + file;
+
+                copyFolder(directoryPath + "/" + file,currentFolder,file );
+                inFolder = true;
+
+
                 //walk
                 //fs.appendFile('Manifest.txt',"Directory :  "+file+"\n",function(error){})
                 //fileWalk(directoryPath + "/" + file);
@@ -48,7 +69,12 @@ async function fileWalkCopy(directoryPath, target){
             }
             //if not a dot file get calcs
             else if (!file.startsWith(".")) {
-              copyFile(file,target);
+                if(inFolder){
+                    copyFile(file,currentFolder);
+                }else{
+                    copyFile(file, repoInNew)
+                }
+              
 
                 //fs.appendFile('Manifest.txt',file + "     " + compute.calc(directoryPath, file)+"\n",function(error){})
             }
@@ -57,6 +83,9 @@ async function fileWalkCopy(directoryPath, target){
         //fs.appendFile('Manifest.txt',"\n\n",function(error){})
     });
 }
-//////////////////////////////////////////////////////////////////////////
-// still need to get a search method to find what manifest to use 
-// pass manifest top level path to the new walk method 
+// givens from user 
+//1- repo folder
+//2- manifest(for now basically the only manifest in the folder assuming files overwrite each other)
+//3- target folder to copy to
+//procedure- use repo folder to copy everything over folders and files
+// then call walk to manifest everything maybe change artifact id's
