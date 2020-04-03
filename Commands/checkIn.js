4ee8d72c91eb3a31de509c9d145282965aaf069e
mlminer1129 <mlminer1129@gmail.com>
2020-04-02 18:39:00 -0700

    commit for pull

--- Commands/checkIn.js
+++ Commands/checkIn.js
@@ -2,15 +2,18 @@
 const fs = require('fs');
 var fileConfig = require('./pathName');
 var walking = require('./walk');
+var copy = require('./copy');
 //can change if label will keep version count
 static var versionCount;
 
 var checkInFile = fs.readFile();
 var previousVersionSize = fs.readFile('Manifest.txt');
+
 function checkIn(checkInFile, previousVersionSize){
 
 //need to maintain version sizes
 var oldSize = fileConfig.calculations(previousVersionSize);
+
 var newManifest = walking.walk(checkInFile);
 //calculate size of manifest from previous version to check for changes being pushed
 ////var sum = fileConfig.calculations(newDirectory);
--- HelperFunctions/copy.js
+++ HelperFunctions/copy.js
@@ -1,3 +1,7 @@
+module.exports ={
+copy: copy
+}
+
 
 function moveDesired(targetFolder, whereTo){
     const fs = require('fs-extra');
