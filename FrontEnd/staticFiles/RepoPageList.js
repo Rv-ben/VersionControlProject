3371ec60b4f407e91032b2cf4a42f3aa6f3134bc
Rv-ben <Rubenbramasco@gmail.com>
2020-04-03 15:02:20 -0700

    Added list support

--- FrontEnd/staticFiles/RepoPageList.js
+++ FrontEnd/staticFiles/RepoPageList.js
@@ -8,4 +8,24 @@ var repo = par.get("Reponame")
 window.onload = function(){
     this.document.getElementById("Title").innerText = repo;
 
+    $.getJSON("http://localhost:5131/json/"+repo,function(data){
+
+    data.Versions.forEach(snapshot => {
+        var listItem = document.createElement("div",{class:"listItem"});
+
+        var textholder = document.createElement("div");
+        textholder.className ="holder";
+
+        var repo = document.createElement("p",{class: "name"});
+        
+        repo.innerText = snapshot.ManifestID;
+        
+        listItem.append(textholder);
+        textholder.append(repo);
+
+        textholder.setAttribute('onclick',"repoPage(this.childNodes[0].innerText)")
+
+        document.getElementById("repoNames").appendChild(listItem);
+    });
+})
 }
