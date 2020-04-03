bfb3783b160732a91ae31d4378b3153dc8b97a1b
mlminer1129 <mlminer1129@gmail.com>
2020-04-02 21:54:27 -0700

    Update with exports
    Just need timestamp

--- Commands/checkIn.js
+++ Commands/checkIn.js
@@ -1,4 +1,7 @@
 //need to pull the previous manifest and the new one
+module.exports={
+	checkIn:checkIn
+}
 const fs = require('fs');
 const fse = require('fs-extra')
 var pathName = require('../HelperFunctions/pathName');
@@ -7,15 +10,22 @@ var copyCurrent = require('../HelperFunctions/copy');
 
 
 //walk
+function checkIn(){
 walking.walk('../Repos/newRepo/Current');
-fse.copySync("Manifest.txt","../Repos/newRepo/Current/Manifest.txt")
-var manID = pathName.calc('../Repos/newRepo/Current', 'Manifest.txt');
-fs.renameSync('../Repos/newRepo/Current/Manifest.txt' , '../Repos/newRepo/Current/' + manID);
-var targetDir = '../Repos/newRepo/Versions/' + manID.replace(".txt", "");
-fs.mkdirSync(targetDir);
-fse.copySync('../Repos/newRepo/Current', targetDir);
-fs.renameSync('../Repos/newRepo/Current/' + manID, '../Repos/newRepo/Current/Manifest.txt');
 
+setTimeout(() => {
+
+	fse.copySync("Manifest.txt","../Repos/newRepo/Current/Manifest.txt")
+	var manID = pathName.calc('../Repos/newRepo/Current', 'Manifest.txt');
+	fs.renameSync('../Repos/newRepo/Current/Manifest.txt' , '../Repos/newRepo/Current/' + manID);
+	var targetDir = '../Repos/newRepo/Versions/' + manID.replace(".txt", "");
+	fs.mkdirSync(targetDir);
+	fse.copySync('../Repos/newRepo/Current', targetDir);
+	fs.renameSync('../Repos/newRepo/Current/' + manID, '../Repos/newRepo/Current/Manifest.txt');
+	
+}, 1500);
+
+}
 //timestamp
 
 
