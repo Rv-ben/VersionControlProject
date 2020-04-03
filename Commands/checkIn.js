83c09faa30be768f4cf25385e781686b2d4ae188
mlminer1129 <mlminer1129@gmail.com>
2020-04-02 22:55:38 -0700

    Double manifest

--- Commands/checkIn.js
+++ Commands/checkIn.js
@@ -7,16 +7,17 @@ const fse = require('fs-extra')
 var pathName = require('../HelperFunctions/pathName');
 var walking = require('../HelperFunctions/walk');
 var copyCurrent = require('../HelperFunctions/copy');
-let date = new Date();
+let fullDate = new Date();
 //need to pull current repo name
-var repoName = 'newRepo';
+var repoName = 'newRepos';
 
-checkIn(repoName);
+checkIn(repoName, fullDate);
 function checkIn(repoName){
 walking.walk('../Repos/' + repoName + '/Current');
 
 setTimeout(() => {
-	//fs.appendFile('../Repos/' + repoName + '/Current/Manifest.txt', date);
+	fs.appendFile('Manifest.txt', fullDate + "\n", function(error){}); 
+	//fs.appendFile('../Repos/' + repoName + '/Current/Manifest.txt', fullDate);
 	fse.copySync("Manifest.txt","../Repos/" + repoName + "/Current/Manifest.txt");
 	var manID = pathName.calc('../Repos/' + repoName + '/Current', 'Manifest.txt');
 	fs.renameSync('../Repos/' + repoName + '/Current/Manifest.txt' , '../Repos/' + repoName + '/Current/' + manID);
