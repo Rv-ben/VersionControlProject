9ec6df73b4170c4fc998d38ebd02f9c79329fe19
ColinRayburn <47834560+ColinRayburn@users.noreply.github.com>
2020-04-04 19:21:19 -0700

    check out final verson working properly

--- HelperFunctions/copy.js
+++ HelperFunctions/copy.js
@@ -1,11 +1,12 @@
 module.exports= {
     copy: copyDesired
 }
+var check = require("../Commands/checkIn")
 var walking = require('../HelperFunctions/walk');
-
+const fs = require('fs-extra');
 
 function moveDesired(targetFolder, whereTo){
-    const fs = require('fs-extra');
+    
     // takes two arguements a folder to move and a destination to move to
     // both arguements should be strings
     fs.moveSync(targetFolder, whereTo + "/"  + targetFolder, (err) => {
@@ -15,38 +16,49 @@ function moveDesired(targetFolder, whereTo){
 }
 
 function copyDesired(folderToCopy, whereTo){
-    const fs = require('fs-extra');
     //takes two arguements the folder in the current directory and the new directory to copy it to
     fs.copySync(folderToCopy, whereTo);
 }
 
 //
 function checkOut(repoFolder, targetFolder, manifest){
-    const fs = require('fs-extra');
-    
-    if(findRepo(repoFolder,manifest) = -1){
-        console.log("repo doesn't exist")
+
+    if(repoFolder + "Versions/" + manifest.isDirectory){
+        fs.copySync('../Repos/' + repoFolder, targetFolder);
+        walking.walk(targetFolder);
+    }else if (manifest ==  "Manifest"){
+        fs.copySync('../Repos/' + repoFolder, targetFolder );
     }else{
-        copyDesired(findRepo(repoFolder,manifest), targetFolder);
-        walking.walk(findRepo(repoFolder,manifest));
+        console.log("maifest didnt exist")
     }
 
+    walking.walk(targetFolder + "/Current");
+    
+    
 }
-// if error happens probably happens here
-function findRepo(directoryPath, targetMani){
-    fs.readdir(directoryPath, function (err, files) {
-        //error checking
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } 
-        //looks for folder 
+
+
+// // if error happens probably happens here
+var directoryPath = "../Repos/newerRepo/";
+var targetMani = "P3636L600C5288";
+var targetFol ="../testCheckOut";
+checkOut(directoryPath, targetFol, targetMani);
+// console.log(findRepo(directoryPath, targetMani));
+
+// function findRepo(directoryPath, targetMani){
+//     fs.readdir(directoryPath, function (err, files) {
+//         //error checking
+//         if (err) {
+//             return console.log('Unable to scan directory: ' + err);
+//         } 
+//         //looks for folder 
         
-        files.forEach(function (file) {
-            if(files.isdirectory == true && file == targetMani){
-                return file;
-            }
-            return -1; 
-        });
-    });
-    };
+//         files.forEach(function (file) {
+//             if(files.isdirectory == true ){
+//                 return file;
+//             }
+//             return -1; 
+//         });
+//     });
+//     };
     
