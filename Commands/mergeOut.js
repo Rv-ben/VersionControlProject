9ee83f336302368bf0655179662b153c12bdb038
ColinRayburn <47834560+ColinRayburn@users.noreply.github.com>
2020-05-09 13:01:03 -0700

    merge out working good

--- Commands/mergeOut.js
+++ Commands/mergeOut.js
@@ -1,56 +1,114 @@
-var checkIn = require('../HelperFunctions/checkIn');
+var checkIn = require('../Commands/checkIn');
 var len = require('../HelperFunctions/pathName');
 var filesSz = require('../HelperFunctions/pathName');
 var fs = require('fs');
+var cop = require('../HelperFunctions/copy');
+var fse = require('fs-extra');
 
-function mergeOut(original, changes, target){
+mergeOut("thisrepo", "P1418L513C9382", "testMergeOut");
 
-    checkIn.checkIn(target);
+function mergeOut(Repo, partOfRepo, target){
+    var vcsPath = "C:/Users/colin/IdeaProjects/tester/src/VersionControlProject/";
+    var gma = vcsPath + "Repos/" + Repo + "/Current";
+    var version = vcsPath + "Repos/" + Repo + "/Versions/" + partOfRepo ;
+    var tar = vcsPath + target;
+   // checkIn.checkIn(tar);
+    
+    //console.log(fs.existsSync(tar + "/Versions/" + partOfRepo));
+    // if(fs.existsSync(tar + "/Versions/" + partOfRepo)){
+    //     var targetfile = tar + "/Versions/" + partOfRepo ;
+    //     var tarRdFile = fs.readFileSync(targetfile , 'utf8'); 
+    //     let tarLines = tarRdFile.split("\r\n");
+    //     var rdVer = fs.readFileSync(version, 'utf8');
+    //     let verLines = rdVer.split("\r\n");
+        
+    //     if(filesSz.fileSz(tarLines) != filesSz.fileSz(verLines)){
+    //         fs.renameSync(targetfile, tar + "/Versions/" + partOfRepo + "/" + partOfRepo + "MT_" +".txt");
+    //         fs.copyFileSync(version, targetfile);
+    //         fs.renameSync(targetfile, tar + "/Versions/" + partOfRepo + "/" + partOfRepo + "MR_" +".txt")
+
+    //         console.log("already there and different");
+    //     }else{
+    //         // in last version of this else wont exist because nothing happens if there already exists a copy of that exact file or folder
+    //         console.log("already there and the same");
+    //     }
+        
+    // }
+    // else{
+    //     if( !fs.exists(tar + "/Versions/", (exists) => {})){
+    //         fs.mkdir(tar + "/Current/", (err) => {})
+    //         fs.mkdir(tar + "/Versions", (err) => {});
+    //     }
+    //     fs.mkdir(tar + "/Versions/" + partOfRepo, (err) => {});
+    //     fse.copySync(version , tar + "/Versions/" + partOfRepo);
+    // }
 
 
-    //walk all files/folders in both files
-    //compare original to changes 
-    // add any new files/ folders 
-    // any colliding folders ( art ids arent the same ) add changes to original and give them a label 
-    //mr for mismatching from repo
-    //mt for mismatch from target
-    files.forEach(function (file1) {
-        var orgFolder = "";
+
+
+
+//rename repo path
+    fs.readdir(version , function (err, files) {
+        //error checking
         if (err) {
             return console.log('Unable to scan directory: ' + err);
         }
+        if( !fs.existsSync(tar + "/Versions/")){
+            fs.mkdir(tar + "/Current/", (err) => {})
+            fs.mkdir(tar + "/Versions", (err) => {});
+        }
+        if(!fs.existsSync(tar + "/Versions/" + partOfRepo)){
+            fs.mkdir(tar + "/Versions/" + partOfRepo, (err) => {});
+        }
+
+    
+    files.forEach(function (file1) {
+        
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+
+        }
         //if is directory 
-        if (fs.statSync(changes + "/" + file1).isDirectory()) {
+        if (fs.statSync(version + "/" + file1).isDirectory()) {
             //walk
-            changesFldr = changes + "/" + file1;
-            orgFolder = original + "/" + file1;
-            if(!fs.exists(tarFolder)){
-                //add folder to target
-                fs.mkdir(orgFolder);
+
+            if(!fs.existsSync(tar + "/Versions/" + partOfRepo)){
+                fs.mkdir(tar + "/Versions/" + partOfRepo, (err) => {});
+                fse.copySync(version , tar + "/Versions/" + partOfRepo);
             }
-            fileWalk(changesFldr);
+            fileWalk(version + "/" + file1);
         }
         //if not a dot file get calcs
         else if (!file1.startsWith(".")) {
-            if(fs.exists(orgFolder + "/" + file1)){
-                //compare the two files and if they are the same dont do anything and if they differ add both with labels
-                var orgRdFile = fs.readFileSync(fullPath+"/"+fileName, 'utf8'); 
-                let orgLines = orgRdFile.split("\r\n");
-                var chgRdFile = fs.readFileSync(fullPath+"/"+fileName, 'utf8'); 
-                let chgLines = chgRdFile.split("\r\n");
-                if(len.len(orgLines) != len.len(chgLines) || filesSz.fileSz(orgLines) != filesSz.fileSz(chgLines)){
-                    fs.rename(orgFolder + "/" + file1, orgFolder + "/" + file1 + "_MR")
-                    fs.rename(chgFolder + "/" + file1, chgFolder + "/" + file1 + "_MT")
 
-                }
+            if(fs.existsSync(tar + "/Versions/" + partOfRepo + "/" + file1)){
+                var targetfile = tar + "/Versions/" + partOfRepo + "/" + file1;
+                var tarRdFile = fs.readFileSync(targetfile , 'utf8'); 
+                let tarLines = tarRdFile.split("\r\n");
+                var rdVer = fs.readFileSync(version, 'utf8');
+                let verLines = rdVer.split("\r\n");
                 
-            }else {
-                 fs.copyFile(chgFolder + "/" + file1, orgFolder + "/");
+                if(filesSz.fileSz(tarLines) != filesSz.fileSz(verLines)){
+                    fs.renameSync(targetfile, tar + "/Versions/" + partOfRepo + "/" + partOfRepo + "MT_");
+                    fs.copyFileSync(version, targetfile);
+                    fs.renameSync(targetfile, tar + "/Versions/" + partOfRepo + "/" + partOfRepo + "MR_");
+                    fse.copySync(gma, targetfile, tar + "/Versions/" + partOfRepo + "/" + "MG_");
+                
+            }
+           
+                    
             }
             
-        }
-    });
-    
+    }
 
-};
-// add everything to a target folder if collisions happen add duplicates 
+ 
+})
+})
+}
+// //need to figure this out to actually get copy to work
+// //fs.copyFileSync("C:/Users/colin/IdeaProjects/tester/src/VersionControlProject/Repos/thisrepo/Versions/P1418L513C9382/P1418L513C9382.txt", "C:/Users/colin/IdeaProjects/tester/src/VersionControlProject/"  +  target + "/P1418L513C9382.txt" );
+
+// // add everything to a target folder 
+// //if collisions happen add duplicates wiht appropriate labels/names
+// // use full path to get to a folder or file in this 
+// // look at checkin for vcs path i think its called fro reference
